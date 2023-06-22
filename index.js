7bdcb77fea75cf339d7b8754d263044cead8ec26
kuanchiliao1 <kuanchiliao@gmail.com>
2023-06-22 10:02:12 -0700

    Set up prettyprint() to display tree

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const Node = (data) => {
     right = value
   }
 
-  const setLeft = (value) {
+  const setLeft = (value) => {
     left = value
   }
 
@@ -37,6 +37,7 @@ const Tree = (array) => {
   const buildTree = () => {
     const node = Node(3)
     node.setLeft(Node(2))
+    node.setRight(Node(399))
     return node
   }
 
@@ -46,4 +47,23 @@ const Tree = (array) => {
   }
 }
 
-console.log(Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]))
+console.log(Tree(
+  [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+).root)
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.getRight() !== null) {
+    prettyPrint(node.getRight(), `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.getData()}`);
+  if (node.getLeft() !== null) {
+    prettyPrint(node.getLeft(), `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+prettyPrint(Tree(
+  [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+).root)
