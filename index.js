f936e87e49147781f90ffbd68e9de18a222834df
kuanchiliao1 <kuanchiliao@gmail.com>
2023-06-22 09:39:15 -0700

    Write outline of Tree factory

--- index.js
+++ index.js
@@ -30,3 +30,20 @@ const Node = (data) => {
     getRight
   }
 };
+
+const Tree = (array) => {
+  let sortedArray = [...new Set(array)].sort((a, b) => a - b)
+
+  const buildTree = () => {
+    const node = Node(3)
+    node.setLeft(Node(2))
+    return node
+  }
+
+  return {
+    root: buildTree(),
+    sortedArray
+  }
+}
+
+console.log(Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]))
